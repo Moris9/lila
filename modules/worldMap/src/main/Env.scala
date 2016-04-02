Moris9/lila

commit ce04b8ea8dfb3ff83e5f522ef1da5b2993dcf658

ensure schedules and subscriptions clear up on actor restart

modules/worldMap/src/main/Env.scala
@@ -17,6 +17,7 @@ final class Env(
     Props(new Stream(
       geoIp = MaxMindIpGeo(GeoIPFile, 0),
       geoIpCacheTtl = GeoIPCacheTtl)))
+  system.lilaBus.subscribe(stream, 'changeFeaturedGame, 'streams, 'nbMembers, 'nbRounds)
 
   def getStream = {
     import play.api.libs.iteratee._

modules/api/src/main/Env.scala
@@ -119,9 +119,9 @@ final class Env(
 
   lazy val cli = new Cli(system.lilaBus, renderer)
 
-  system.actorOf(Props(new KamonPusher(
-    countUsers = () => userEnv.onlineUserIdMemo.count
-  )))
+  KamonPusher.start(system) {
+    new KamonPusher(countUsers = () => userEnv.onlineUserIdMemo.count)
+  }
 }
 
 object Env {

modules/explorer/src/main/Env.scala
@@ -30,12 +30,11 @@ final class Env(
     }
   }
 
-  if (IndexFlow) system.actorOf(Props(new Actor {
-    context.system.lilaBus.subscribe(self, 'finishGame)
+  if (IndexFlow) system.lilaBus.subscribe(system.actorOf(Props(new Actor {
     def receive = {
       case lila.game.actorApi.FinishGame(game, _, _) => indexer(game)
     }
-  }))
+  })), 'finishGame)
 }
 
 object Env {

modules/gameSearch/src/main/Env.scala
@@ -28,14 +28,13 @@ final class Env(
     forms = forms,
     paginator = paginator)
 
-  system.actorOf(Props(new Actor {
+  system.lilaBus.subscribe(system.actorOf(Props(new Actor {
     import lila.game.actorApi.{ InsertGame, FinishGame }
-    context.system.lilaBus.subscribe(self, 'finishGame)
     def receive = {
       case FinishGame(game, _, _) => self ! InsertGame(game)
       case InsertGame(game)       => api store game
     }
-  }), name = ActorName)
+  }), name = ActorName), 'finishGame)
 
   def cli = new lila.common.Cli {
     import akka.pattern.ask

modules/insight/src/main/Env.scala
@@ -46,12 +46,11 @@ final class Env(
     pipeline = aggregationPipeline,
     indexer = indexer)
 
-  system.actorOf(Props(new Actor {
-    system.lilaBus.subscribe(self, 'analysisReady)
+  system.lilaBus.subscribe(system.actorOf(Props(new Actor {
     def receive = {
       case lila.analyse.actorApi.AnalysisReady(game, _) => api updateGame game
     }
-  }))
+  })), 'analysisReady)
 }
 
 object Env {

modules/lobby/src/main/Env.scala
@@ -44,15 +44,16 @@ final class Env(
     maxPerPage = SeekMaxPerPage,
     maxPerUser = SeekMaxPerUser)
 
-  val lobby = system.actorOf(Props(new Lobby(
-    socket = socket,
-    seekApi = seekApi,
-    blocking = blocking,
-    playban = playban,
-    onStart = onStart,
+  val lobby = Lobby.start(system, ActorName,
     broomPeriod = BroomPeriod,
-    resyncIdsPeriod = ResyncIdsPeriod
-  )), name = ActorName)
+    resyncIdsPeriod = ResyncIdsPeriod) {
+      new Lobby(
+        socket = socket,
+        seekApi = seekApi,
+        blocking = blocking,
+        playban = playban,
+        onStart = onStart)
+    }
 
   lazy val socketHandler = new SocketHandler(
     hub = hub,
@@ -64,13 +65,12 @@ final class Env(
 
   private val abortListener = new AbortListener(seekApi = seekApi)
 
-  system.actorOf(Props(new Actor {
-    system.lilaBus.subscribe(self, 'abortGame)
+  system.lilaBus.subscribe(system.actorOf(Props(new Actor {
     def receive = {
       case lila.game.actorApi.AbortedBy(pov) if pov.game.isCorrespondence =>
         abortListener recreateSeek pov
     }
-  }))
+  })), 'abortGame)
 }
 
 object Env {

modules/mod/src/main/Env.scala
@@ -65,10 +65,7 @@ final class Env(
     emailAddress = emailAddress)
 
   // api actor
-  private val actorApi = system.actorOf(Props(new Actor {
-    override def preStart {
-      context.system.lilaBus.subscribe(self, 'finishGame, 'analysisReady)
-    }
+  system.lilaBus.subscribe(system.actorOf(Props(new Actor {
     def receive = {
       case lila.hub.actorApi.mod.MarkCheater(userId) => api autoAdjust userId
       case lila.analyse.actorApi.AnalysisReady(game, analysis) =>
@@ -79,7 +76,7 @@ final class Env(
             assessApi.onGameReady(game, whiteUser, blackUser)
         }
     }
-  }), name = ActorName)
+  }), name = ActorName), 'finishGame, 'analysisReady)
 }
 
 object Env {

modules/perfStat/src/main/Env.scala
@@ -35,12 +35,11 @@ final class Env(
       indexer.userPerf(user, perfType) >> storage.find(user.id, perfType)
     } map (_ | PerfStat.init(user.id, perfType))
 
-  system.actorOf(Props(new Actor {
-    context.system.lilaBus.subscribe(self, 'finishGame)
+  system.lilaBus.subscribe(system.actorOf(Props(new Actor {
     def receive = {
       case lila.game.actorApi.FinishGame(game, _, _) => indexer addGame game
     }
-  }))
+  })), 'finishGame)
 }
 
 object Env {

commit 1beed62afa791535ae202abce25218c2b73febab

fix network map
regression from
https://github.com/ornicar/lila/commit/ce04b8ea8dfb3ff83e5f522ef1da5b2993dcf658#diff-07658aa52e35f7a1edc5fcd621377352
couldn't see any other copypaste mistake

modules/worldMap/src/main/Env.scala
@@ -17,7 +17,7 @@ final class Env(
     Props(new Stream(
       geoIp = MaxMindIpGeo(GeoIPFile, 0),
       geoIpCacheTtl = GeoIPCacheTtl)))
-  system.lilaBus.subscribe(stream, 'changeFeaturedGame, 'streams, 'nbMembers, 'nbRounds)
+  system.lilaBus.subscribe(stream, 'roundDoor)
 
   def getStream = {
     import play.api.libs.iteratee._