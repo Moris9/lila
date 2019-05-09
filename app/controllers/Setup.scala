Moris9/lila

commit d8b5c3b4d1337d86f20f0dea79db81e36b0f084d

more template rewrites

app/controllers/Setup.scala
@@ -12,8 +12,8 @@ import lila.common.{ HTTPRequest, LilaCookie, IpAddress }
 import lila.game.{ GameRepo, Pov, AnonCookie }
 import lila.setup.Processor.HookResult
 import lila.setup.ValidFen
-import lila.user.UserRepo
 import lila.socket.Socket.Uid
+import lila.user.UserRepo
 import views._
 
 object Setup extends LilaController with TheftPrevention {
@@ -69,10 +69,12 @@ object Setup extends LilaController with TheftPrevention {
         ), {
           case config => userId ?? UserRepo.byId flatMap { destUser =>
             destUser ?? { Env.challenge.granter(ctx.me, _, config.perfType) } flatMap {
-              case Some(denied) => negotiate(
-                html = BadRequest(html.challenge.denied(denied)).fuccess,
-                api = _ => BadRequest(jsonError(lila.challenge.ChallengeDenied.translated(denied))).fuccess
-              )
+              case Some(denied) =>
+                val message = lila.challenge.ChallengeDenied.translated(denied)
+                negotiate(
+                  html = BadRequest(message).fuccess,
+                  api = _ => BadRequest(jsonError(message)).fuccess
+                )
               case None =>
                 import lila.challenge.Challenge._
                 val challenge = lila.challenge.Challenge.make(

app/controllers/Insight.scala
@@ -27,7 +27,7 @@ object Insight extends LilaController {
     Accessible(username) { user =>
       import lila.insight.InsightApi.UserStatus._
       env.api userStatus user flatMap {
-        case NoGame => Ok(html.insight.noGame(user)).fuccess
+        case NoGame => Ok(html.site.message.insightNoGames(user)).fuccess
         case Empty => Ok(html.insight.empty(user)).fuccess
         case s => for {
           cache <- env.api userCache user

app/controllers/LilaController.scala
@@ -216,16 +216,16 @@ private[controllers] trait LilaController
     else res
 
   protected def NoEngine[A <: Result](a: => Fu[A])(implicit ctx: Context): Fu[Result] =
-    if (ctx.me.exists(_.engine)) Forbidden(views.html.site.noEngine()).fuccess else a
+    if (ctx.me.exists(_.engine)) Forbidden(views.html.site.message.noEngine).fuccess else a
 
   protected def NoBooster[A <: Result](a: => Fu[A])(implicit ctx: Context): Fu[Result] =
-    if (ctx.me.exists(_.booster)) Forbidden(views.html.site.noBooster()).fuccess else a
+    if (ctx.me.exists(_.booster)) Forbidden(views.html.site.message.noBooster).fuccess else a
 
   protected def NoLame[A <: Result](a: => Fu[A])(implicit ctx: Context): Fu[Result] =
     NoEngine(NoBooster(a))
 
   protected def NoBot[A <: Result](a: => Fu[A])(implicit ctx: Context): Fu[Result] =
-    if (ctx.me.exists(_.isBot)) Forbidden(views.html.site.noBot()).fuccess else a
+    if (ctx.me.exists(_.isBot)) Forbidden(views.html.site.message.noBot).fuccess else a
 
   protected def NoLameOrBot[A <: Result](a: => Fu[A])(implicit ctx: Context): Fu[Result] =
     NoLame(NoBot(a))

app/controllers/Report.scala
@@ -16,7 +16,7 @@ object Report extends LilaController {
   private def api = env.api
 
   def list = Secure(_.SeeReport) { implicit ctx => me =>
-    if (Env.streamer.liveStreamApi.isStreaming(me.id)) fuccess(Forbidden(html.mod.streaming()))
+    if (Env.streamer.liveStreamApi.isStreaming(me.id)) fuccess(Forbidden(html.site.message.streamingMod))
     else renderList(env.modFilters.get(me).fold("all")(_.key))
   }
 

app/controllers/Study.scala
@@ -402,7 +402,7 @@ object Study extends LilaController {
   private def CanViewResult(study: StudyModel)(f: => Fu[Result])(implicit ctx: lila.api.Context) =
     if (canView(study)) f
     else negotiate(
-      html = fuccess(Unauthorized(html.study.restricted(study))),
+      html = fuccess(Unauthorized(html.site.message.privateStudy(study.ownerId))),
       api = _ => fuccess(Unauthorized(jsonError("This study is now private")))
     )
 

app/controllers/Team.scala
@@ -214,7 +214,7 @@ object Team extends LilaController {
 
   private def OnePerWeek[A <: Result](me: UserModel)(a: => Fu[A])(implicit ctx: Context): Fu[Result] =
     api.hasCreatedRecently(me) flatMap { did =>
-      if (did && !Granter(_.SuperAdmin)(me)) Forbidden(views.html.team.createLimit()).fuccess
+      if (did && !Granter(_.SuperAdmin)(me)) Forbidden(views.html.site.message.teamCreateLimit).fuccess
       else a
     }
 

app/templating/AssetHelper.scala
@@ -115,6 +115,10 @@ trait AssetHelper { self: I18nHelper with SecurityHelper =>
     val nonce = ctx.nonce ?? { nonce => s""" nonce="$nonce"""" }
     s"""<script$nonce>$js</script>"""
   }
+  def embedJsUnsafe(js: scalatags.Text.RawFrag)(implicit ctx: Context): scalatags.Text.RawFrag = scalatags.Text.all.raw {
+    val nonce = ctx.nonce ?? { nonce => s""" nonce="$nonce"""" }
+    s"""<script$nonce>$js</script>"""
+  }
 
   def embedJs(js: Html)(implicit ctx: Context): Html = embedJsUnsafe(js.body)
 }

app/templating/Environment.scala
@@ -28,7 +28,8 @@ object Environment
   with AnalysisHelper
   with TournamentHelper
   with SimulHelper
-  with ChessgroundHelper {
+  with ChessgroundHelper
+  with ui.Scalatags {
 
   implicit val LilaHtmlMonoid = scalaz.Monoid.instance[Html](
     (a, b) => Html(a.body + b.body),

commit 549161fed043c62690d3f95939091bec2710ef5f

fix challenge denied rendered as pure text
Regression in d8b5c3b4d1337d86f20f0dea79db81e36b0f084d.

app/controllers/Setup.scala
@@ -73,7 +73,7 @@ object Setup extends LilaController with TheftPrevention {
             case Some(denied) =>
               val message = lila.challenge.ChallengeDenied.translated(denied)
               negotiate(
-                html = BadRequest(message).fuccess,
+                html = BadRequest(html.site.message.challengeDenied(message)).fuccess,
                 api = _ => BadRequest(jsonError(message)).fuccess
               )
             case None =>

modules/challenge/src/main/ChallengeGranter.scala
@@ -21,7 +21,7 @@ object ChallengeDenied {
     case object FriendsOnly extends Reason
   }
 
-  def translated(d: ChallengeDenied)(implicit lang: lila.common.Lang) = d.reason match {
+  def translated(d: ChallengeDenied)(implicit lang: lila.common.Lang): String = d.reason match {
     case Reason.YouAreAnon => I18nKeys.registerToSendChallenges.txt()
     case Reason.YouAreBlocked => I18nKeys.youCannotChallengeX.txt(d.dest.titleUsername)
     case Reason.TheyDontAcceptChallenges => I18nKeys.xDoesNotAcceptChallenges.txt(d.dest.titleUsername)