Moris9/lila

commit dda3f649dd5d604b3c695aef05a0b9e7f545acd1

Remove unused things

modules/racer/src/main/Env.scala
@@ -4,30 +4,20 @@ import com.softwaremill.macwire.*
 
 import lila.common.LightUser
 import lila.db.AsyncColl
-import lila.storm.{ StormJson, StormSelector, StormSign }
+import lila.storm.StormSelector
 
 @Module
-@annotation.nowarn("msg=unused")
 final class Env(
     selector: StormSelector,
-    puzzleColls: lila.puzzle.PuzzleColls,
     cacheApi: lila.memo.CacheApi,
-    stormJson: StormJson,
-    stormSign: StormSign,
     userRepo: lila.user.UserRepo,
-    lightUserGetter: LightUser.GetterSyncFallback,
-    remoteSocketApi: lila.socket.RemoteSocket,
-    db: lila.db.Db
+    lightUserGetter: LightUser.GetterSyncFallback
 )(using Executor, Scheduler, play.api.Mode):
-
-  private lazy val colls = RacerColls(puzzle = puzzleColls.puzzle)
 
   lazy val api = wire[RacerApi]
 
   lazy val lobby = wire[RacerLobby]
 
   lazy val json = wire[RacerJson]
-
-  private val socket = wire[RacerSocket] // requires eager eval
 
 final private class RacerColls(val puzzle: AsyncColl)

app/Env.scala
@@ -146,7 +146,6 @@ final class Env(
   lazy val gamePaginator = wire[mashup.GameFilterMenu.PaginatorBuilder]
   lazy val pageCache     = wire[http.PageCache]
 
-  @annotation.nowarn("msg=unused")
   private val tryDailyPuzzle: lila.puzzle.DailyPuzzle.Try = () =>
     Future {
       puzzle.daily.get
@@ -159,7 +158,6 @@ final class Env(
   system.actorOf(Props(new templating.RendererActor), name = config.get[String]("hub.actor.renderer"))
 end Env
 
-@annotation.nowarn("msg=unused")
 final class EnvBoot(
     config: Configuration,
     environment: Environment,

modules/activity/src/main/Env.scala
@@ -7,7 +7,6 @@ import lila.common.config.*
 import lila.hub.actorApi.round.CorresMoveEvent
 
 @Module
-@annotation.nowarn("msg=unused")
 final class Env(
     db: lila.db.AsyncDb @@ lila.db.YoloDb,
     practiceApi: lila.practice.PracticeApi,

modules/analyse/src/main/Env.scala
@@ -5,7 +5,6 @@ import com.softwaremill.macwire.*
 import lila.common.config.*
 
 @Module
-@annotation.nowarn("msg=unused")
 final class Env(
     db: lila.db.Db,
     gameRepo: lila.game.GameRepo,

modules/api/src/main/Env.scala
@@ -13,7 +13,6 @@ import lila.hub.actorApi.lpv.*
 import lila.user.User
 
 @Module
-@annotation.nowarn("msg=unused")
 final class Env(
     appConfig: Configuration,
     net: NetConfig,
@@ -24,7 +23,6 @@ final class Env(
     forumEnv: lila.forum.Env,
     teamEnv: lila.team.Env,
     puzzleEnv: lila.puzzle.Env,
-    explorerEnv: lila.explorer.Env,
     fishnetEnv: lila.fishnet.Env,
     studyEnv: lila.study.Env,
     studySearchEnv: lila.studySearch.Env,
@@ -58,7 +56,6 @@ final class Env(
     ublogApi: lila.ublog.UblogApi,
     picfitUrl: lila.memo.PicfitUrl,
     cacheApi: lila.memo.CacheApi,
-    mongoCacheApi: lila.memo.MongoCache.Api,
     ws: StandaloneWSClient,
     val mode: Mode
 )(using

modules/appeal/src/main/Env.scala
@@ -5,7 +5,6 @@ import com.softwaremill.macwire.*
 import lila.common.config.*
 
 @Module
-@annotation.nowarn("msg=unused")
 final class Env(
     db: lila.db.Db,
     noteApi: lila.user.NoteApi,

modules/blog/src/main/Env.scala
@@ -11,7 +11,6 @@ private class BlogConfig(
 )
 
 @Module
-@annotation.nowarn("msg=unused")
 final class Env(
     appConfig: Configuration,
     timelineApi: lila.timeline.EntryApi,

modules/bookmark/src/main/Env.scala
@@ -15,7 +15,6 @@ final private class BookmarkConfig(
 )
 
 @Module
-@annotation.nowarn("msg=unused")
 final class Env(
     appConfig: Configuration,
     db: lila.db.Db,

commit 306824320032cd688e988c0c24b8f438d6852c5d

fix racer websocket connection - broken by dda3f649

modules/racer/src/main/Env.scala
@@ -4,20 +4,30 @@ import com.softwaremill.macwire.*
 
 import lila.common.LightUser
 import lila.db.AsyncColl
-import lila.storm.StormSelector
+import lila.storm.{ StormJson, StormSelector, StormSign }
 
 @Module
+@annotation.nowarn("msg=unused")
 final class Env(
     selector: StormSelector,
+    puzzleColls: lila.puzzle.PuzzleColls,
     cacheApi: lila.memo.CacheApi,
+    stormJson: StormJson,
+    stormSign: StormSign,
     userRepo: lila.user.UserRepo,
-    lightUserGetter: LightUser.GetterSyncFallback
+    lightUserGetter: LightUser.GetterSyncFallback,
+    remoteSocketApi: lila.socket.RemoteSocket,
+    db: lila.db.Db
 )(using Executor, Scheduler, play.api.Mode):
+
+  private lazy val colls = RacerColls(puzzle = puzzleColls.puzzle)
 
   lazy val api = wire[RacerApi]
 
   lazy val lobby = wire[RacerLobby]
 
   lazy val json = wire[RacerJson]
+
+  wire[RacerSocket] // requires eager eval
 
 final private class RacerColls(val puzzle: AsyncColl)